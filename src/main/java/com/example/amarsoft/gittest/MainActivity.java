62fdcee2c858dd74137afad0d1537a0fc44e8a25
myli1 <254953049@qq.com>
2017-04-14 11:14:17 +0800

    初始化控件。

--- src/main/java/com/example/amarsoft/gittest/MainActivity.java
+++ src/main/java/com/example/amarsoft/gittest/MainActivity.java
@@ -3,15 +3,22 @@ package com.example.amarsoft.gittest;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 /**
  * Created by amarsoft on 2017/4/14.
  */
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mTextView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mTextView = (TextView) findViewById(R.id.textView);
     }
 }
